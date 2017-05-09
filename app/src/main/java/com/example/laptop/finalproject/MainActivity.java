90a038e48b520eab210be35e94b5391efe2f20a4
jedimg <reptilepz@gmail.com>
2017-05-09 09:44:46 +0100

    added the map activity

--- app/src/main/java/com/example/laptop/finalproject/MainActivity.java
+++ app/src/main/java/com/example/laptop/finalproject/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.laptop.finalproject;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +9,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /**
+         * TODO: Create and implement views that let the user provide input for the API and a way
+         * to get to the MapsActivity
+         */
+
+        /*Testing the map activity
+        Intent intent = new Intent(getBaseContext(), MapsActivity.class);
+        startActivity(intent);
+        */
     }
 }
---
+++ app/src/main/java/com/example/laptop/finalproject/MapsActivity.java
@@ -0,0 +1,58 @@
+package com.example.laptop.finalproject;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+//This will be the main activity that controls and communicates with all the fragments
+//It is called from the MainActivity after the user has defined what types of restaurants
+//should be displayed
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        //Calls a method which populates the map based on user input passed from MainActivity
+        //and data retreived from the API.
+        populateMap();
+
+        // Add a marker in Sydney and move the camera
+        /*LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));*/
+    }
+
+    private void populateMap() {
+
+        /**
+         * TODO: Implement the method which populates he map with markers based on user input
+         */
+    }
+}
